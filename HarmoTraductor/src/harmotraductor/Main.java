10dec0c89fe1f8174c8f83506ecf0dfb2e1c5478
vlad29 <vladevil@gmail.com>
2011-05-31 15:55:26 +0200

    2011-05-31-15:55:25

--- HarmoTraductor/src/harmotraductor/Main.java
+++ HarmoTraductor/src/harmotraductor/Main.java
@@ -7,10 +7,61 @@ package harmotraductor;
 
 import java.io.*;
 import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.TextField.*;
+import javax.swing.JFrame.*;
 /**
  *
  * @author alu03009
  */
+
+
+
+class formulario extends JFrame
+{
+    JMenuBar barraMenu = new JMenuBar();
+    JTextArea notastxt = new JTextArea(2,5);
+    String texto = " ";
+    public ArrayList <JLabel> imnotas = new <JLabel> ArrayList(58);
+   
+
+    void crearimages(ArrayList a)
+    {
+        int i = 0;
+
+        System.out.println(a.size()+" "+imnotas.size());
+        for(i=0;i<a.size();i++)
+        {
+           imnotas.add(new JLabel(""));
+           (imnotas.get(i)).setIcon(new ImageIcon(((Nota)(a.get(i))).getpath()));
+        }
+    }
+
+    void dentrotexto(String a)
+       {
+             notastxt.setText(a) ;
+       }
+
+    void cargar()
+    {
+        notastxt.setLineWrap(true);
+        this.setLayout(new FlowLayout());
+        this.add(notastxt);
+        this.setJMenuBar(barraMenu);
+        int j = 0;
+        for(j=0;j<imnotas.size();j++)
+        {
+            System.out.println("anadido icon - "+j);
+            this.add(imnotas.get(j));
+        }
+        this.setSize(400, 500);
+        this.setVisible(true);
+    }
+}
+
+
+
 public class Main {
 
     /**
@@ -23,31 +74,19 @@ public class Main {
         String[] salida;
         salida = f.list();
         String archivo = dir+salida[0];
-
-
-          /*   int i = 0;
-
-        for(i=0;i<salida.length;i++)
-        {
-            File r = new File(dir+salida[i]);
-            if(r.isDirectory())
-            {
-                System.out.println("<DIR>"+salida[i]);
-            }
- else
-            {
-                System.out.println("<FILE>"+salida[i]);
-
-            }
-        }*/
+        String fichero = new String();
+        char head = 'a';
+        formulario form = new formulario();
+        Partitura Cancion = new Partitura();
 
         try{
         FileReader cabezal = new FileReader(archivo);
         BufferedReader bf = new BufferedReader(cabezal);
 
-        Partitura Cancion = new Partitura();
+        
         int notatube;
         while((notatube = bf.read())!=-1){
+
         if(notatube!=32&&notatube!=41)
         {
 
@@ -71,13 +110,24 @@ public class Main {
         }
         }
 
+
+        
+        
+        
+
+        form.dentrotexto(Cancion.toString());
+
+
+
+
         }
    	catch (Exception ex_3)
 	{
 	System.out.println (ex_3.getMessage());
         }
-
-
+        form.crearimages(Cancion.notas);
+        form.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        form.cargar();
 
 
     }
@@ -111,12 +161,21 @@ class Partitura
        notas.add(index,dupli);
     }
 
+   public String toString()
+    {
+       String prev = notas.toString();
+       prev = prev.replace("[\n,"," ");
+       prev = prev.replace(',',' ');
+       prev = prev.replace("\n]","");
+       return prev;
+   }
 }
 
 class Nota
 {
     int numeronota;
     int modificador;
+    String imagename;
 
     Nota(int a)
     {
@@ -124,11 +183,39 @@ class Nota
         {
             numeronota=a;
             modificador=0;
+            imagename = "./images/"+numeronota+".gif";
+            System.out.println(imagename);
         }
         else
         {
             numeronota=(a*-1);
             modificador=-1;
+            imagename = "./images/("+numeronota+").gif";
+            System.out.println(imagename);
+        }
+    }
+
+    public String getpath()
+    {
+        return imagename;
+    }
+
+    public String toString()
+    {
+        if(this.numeronota==120)
+        {
+            return "\n";
+        }
+ else
+        {
+        if(this.modificador==-1)
+        {
+          return "(-"+this.numeronota+") ";
+        }
+        else
+        {
+            return "("+this.numeronota+") ";
+        }
         }
     }
 }
